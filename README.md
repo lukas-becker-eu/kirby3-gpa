dfec9ec4afbd8530433131acdffe4e630cfc0590
Lukas Becker <lukas@candyblue.agency>
2020-11-13 21:56:03 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Kirby 3 Plugin Starterkit
+# Kirby 3 Google My Business
 
-1. Clone this repo with ```$ git clone https://github.com/wearecandyblue/kirby3-plugin.git <your-new-project-directory>```
-2. ```$ git remote set-url origin <your-new-git-repo-url>```
-3. ```$ git submodule add https://github.com/wearecandyblue/kirby3-gtm.git site/plugins/gtm```
+Place ID:
+API Key:
+
+
+Go to ... and enable Google Places Api
--- index.php
+++ index.php
@@ -2,6 +2,35 @@
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-Kirby::plugin('wearecandyblue/plugin', [
-
+Kirby::plugin('candyblue/kirby3-gpa', [
+  'options' => [
+    'key' => '',
+    'place' => '',
+  ]
 ]);
+
+function gpaData() {
+  if(!empty(option('candyblue.kirby3-gpa.key')) && !empty(option('candyblue.kirby3-gpa.place'))) {
+    $key = option('candyblue.kirby3-gpa.key');
+    $place = option('candyblue.kirby3-gtm.place');
+    $json_url = "https://maps.googleapis.com/maps/api/place/details/json?placeid={$place}&fields=user_ratings_total,rating&key={$key}";
+    $json_content = file_get_contents($json_url);
+    $json_data = json_decode($json_content, true);
+
+    return $json_data;
+  }
+}
+
+function gpaRating() {
+  if(!empty(gpaData())) {
+    $rating = gpaData()['result']['rating'];
+    return $rating;
+  }
+}
+
+function gpaUserRatingsTotal() {
+  if(!empty(gpaData())) {
+    $user_ratings_total = gpaData()['result']['user_ratings_total'];
+    return $user_ratings_total;
+  }
+}
